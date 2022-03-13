d2c8856ffa01461f21f1ac4c6097a18e0b56b134
RokerDev <rokerdev91@gmail.com>
2022-03-13 22:47:53 +0100

    refactor: change tile class to represent single tile

--- tile.py
+++ tile.py
@@ -1,34 +1,19 @@
 import random
 from turtle import Turtle
 
+tile_colors = ["Red", "Blue", "Yellow"]
+
 
 class Tile(Turtle):
 
-    def __init__(self, screen_width, screen_height):
+    def __init__(self):
         super().__init__()
-        self.tiles = []
-        self.colors = ["yellow", "blue", "green"]
-        self.levels = 4
-        self.height_position = int(screen_height / 2 - 100)
-        self.width_position = int(screen_width / 2)
+        self.hideturtle()
+        self.shape("square")
+        self.shapesize(stretch_len=2.5)
+        self.penup()
+        self.color(random.choice(tile_colors))
+        self.speed(0)
 
-        self.create_tiles()
-
-    def create_tiles(self):
-        for pos_y in range(0, self.levels):
-            for pos_x in range(-self.width_position + 25, self.width_position, 54):
-                self.create_tile((pos_x, self.height_position))
-            self.height_position -= 24
-
-    def create_tile(self, position):
-        tile = Turtle(shape="square")
-        print(tile.turtlesize())
-        tile.shapesize(stretch_len=2.5)
-        print(tile.turtlesize())
-        tile.penup()
-        tile.hideturtle()
-        tile.color(random.choice(self.colors))
-        tile.speed(0)
-        tile.setposition(position)
-        tile.showturtle()
-        self.tiles.append(tile)
+    def broken_tile(self):
+        self.hideturtle()
