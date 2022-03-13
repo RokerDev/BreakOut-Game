9dd742aac05c6105ef257671f7bddc23811c5d00
RokerDev <rokerdev91@gmail.com>
2022-03-13 13:48:48 +0100

    Initial Commit - Set Game Window

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(600, 800)
+screen.bgcolor("black")
+screen.title("BlackOut Game")
+# screen.tracer(0)
+
+
+
+screen.exitonclick()
