01dae9b9b21f68ac3d5a507806f7c9acc35685cb
RokerDev <rokerdev91@gmail.com>
2022-03-13 22:46:31 +0100

    feat: add a Tile Manager class manage creating tiles

---
+++ tile_manager.py
@@ -0,0 +1,35 @@
+from tile import Tile
+
+
+class TileManager:
+
+    def __init__(self):
+        self.tiles = []
+        self.actual_pos_y = 300
+        self.actual_pos_x = -275
+        self.end_pos_x = 275
+        self.space_between = 4
+        self.tile_width = 50
+        self.tile_height = 20
+        self.create_tile_board()
+        self.show_board()
+
+    def create_tile(self, position):
+        tile = Tile()
+        tile.setposition(position)
+        tile.showturtle()
+        self.tiles.append(tile)
+
+    def create_tile_board(self):
+        for pos_y in range(4):
+            self.create_tile((self.actual_pos_x, self.actual_pos_y))
+            for pos_x in range(10):
+                # print(self.actual_pos_x, self.actual_pos_y)
+                self.actual_pos_x += self.space_between + self.tile_width
+                self.create_tile((self.actual_pos_x, self.actual_pos_y))
+            self.actual_pos_y -= self.space_between + self.tile_height
+            self.actual_pos_x = -275
+
+    def show_board(self):
+        for tile in self.tiles:
+            tile.showturtle()
