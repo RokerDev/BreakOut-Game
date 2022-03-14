77710c3d3033a72c066df82c9805139a2db90e70
RokerDev <rokerdev91@gmail.com>
2022-03-14 17:01:39 +0100

    feat: add setting ball starting position and heading direction

--- ball.py
+++ ball.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle
 
 
@@ -8,15 +9,19 @@ class Ball(Turtle):
         self.hideturtle()
         self.color("white")
         self.shape("circle")
-        self.setheading(90)
-        self.setposition(0, -180)
+        self.set_ball()
+        self.speed(1)
         self.showturtle()
 
     def move(self):
-        self.forward(2)
+        self.forward(4)
 
     def bounce_y(self):
-        self.setheading(360 - self.heading())
+        self.setheading(360 - self.heading() + random.randint(-5, 5))
 
     def bounce_x(self):
-        self.setheading(180 - self.heading())
+        self.setheading(180 - self.heading() + random.randint(-5, 5))
+
+    def set_ball(self):
+        self.setposition(0, -180)
+        self.setheading(random.randint(70, 110))
