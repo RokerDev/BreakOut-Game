c6e0a4ce5e56e48dcce053058806fcbe25f5fd06
RokerDev <rokerdev91@gmail.com>
2022-03-16 18:00:39 +0100

    refactor: change ball start position and add changer to bouncing func

--- ball.py
+++ ball.py
@@ -13,15 +13,21 @@ class Ball(Turtle):
         self.speed(1)
         self.showturtle()
 
-    def move(self):
-        self.forward(4)
+    def move(self, move=1):
+        self.forward(move)
 
-    def bounce_y(self):
-        self.setheading(360 - self.heading() + random.randint(-5, 5))
+    def bounce_y(self, changer=0):
+        self.setheading(360 - self.heading() + changer)
 
-    def bounce_x(self):
-        self.setheading(180 - self.heading() + random.randint(-5, 5))
+    def bounce_x(self, changer=0):
+        self.setheading(180 - self.heading() + changer)
 
     def set_ball(self):
-        self.setposition(0, -180)
+        self.setposition(0, -360)
         self.setheading(random.randint(70, 110))
+
+    def set_mouse_ball(self, event):
+        ball_x = -301 + event.x
+        ball_y = 400 - event.y
+        self.setx(ball_x)
+        self.sety(ball_y)
