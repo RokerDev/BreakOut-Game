799c903cd4c72a2be4a523ed8f58b099d427fdca
RokerDev <rokerdev91@gmail.com>
2022-03-16 19:20:03 +0100

    feat: add scoreboard class that counting and displaying points

---
+++ scoreboard.py
@@ -0,0 +1,43 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.logo = "BreakOut Game"
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.write_logo()
+        self.draw_logo_bottom_line()
+        self.draw_top_line()
+        self.scores = Turtle(visible=False)
+        self.scores.color("white")
+        self.scores.penup()
+        self.show_score()
+
+    def write_logo(self):
+        self.setposition(-240, 320)
+        self.write(self.logo, font=("Verdana", 45, "normal"))
+
+    def draw_logo_bottom_line(self):
+        self.setposition(-300, 300)
+        self.pendown()
+        self.goto(300, 300)
+        self.penup()
+
+    def draw_top_line(self):
+        self.goto(-300, 200)
+        self.pendown()
+        self.goto(300, 200)
+        self.penup()
+
+    def show_score(self):
+        self.scores.clear()
+        self.scores.setposition(-120, 220)
+        self.scores.write(f"Score: {self.score} ", font=("Verdana", 40, "normal"))
+
+    def add_points(self):
+        self.score += 1
